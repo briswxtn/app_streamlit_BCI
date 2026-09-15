458708788e9d6610b8340ce148fc4ea5a863113f
briswxtn <brisscarrasco24@gmail.com>
2026-09-15 08:34:23 -0700

    Correct temperature condition handling in app.py
    Fix syntax errors in temperature condition checks.

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ temperatura = st.number_input(
   "Introduce la temperatura en °C:",
   value=20
 )
-st.writte:
-
-if temperatura<20:
+st.writte(if temperatura<20:
   print("hace frío")
 elif temperatura<30:
   print("la temperatura es agradable")
 else:
-  print("hace calor")
+  print("hace calor") 
+  )
   
