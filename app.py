dfb6704a6600361c0844226921eeb57ccc0f774f
briswxtn <brisscarrasco24@gmail.com>
2026-09-15 08:50:26 -0700

    Fix temperature classification output method

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import streamlit as st
 st.title("Mi aplicación Programación")
 st.button("hacer click aquí")
 st.balloons()
+
 st.title("Clasificador de temperatura")
 temperatura = st.number_input(
   "Introduce la temperatura en °C:",
   value=20
 )
-st.writte(if temperatura<20:
-  print("hace frío")
+if temperatura<20:
+  st.writte("hace frío")
 elif temperatura<30:
-  print("la temperatura es agradable")
+  st.writte("la temperatura es agradable")
 else:
-  print("hace calor") 
+ st.writte("hace calor") 
   )
-  
+
+ st.title("Control de acceso")
+
