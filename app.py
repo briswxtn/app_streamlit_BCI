44a68b033d1a515d0675f3770f00e05d1c21e972
briswxtn <brisscarrasco24@gmail.com>
2026-09-15 08:29:06 -0700

    Add temperature classifier to app.py

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ import streamlit as st
 st.title("Mi aplicación Programación")
 st.button("hacer click aquí")
 st.balloons()
+st.title("Clasificador de temperatura")
+temperatura = st.number_input(
+  "Introduce la temperatura en °C:",
+  value=20
+)
+if temperatura<20:
+  print("hace frío")
+elif temperatura<30:
+  print("la temperatura es agradable")
+else:
+  print("hace calor")
+  
